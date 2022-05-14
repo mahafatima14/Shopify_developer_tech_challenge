10ab354df043928d2bb0e6e300eeff1e4dd4fea4
Your Name <mahafatima14@gmail.com>
2022-05-14 02:25:39 +0000

    first commit

---
+++ server.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request, flash, session, redirect
+from model import connect_to_db, db
+from datetime import datetime
+import crud
+
+# configure a Jinja2 setting to make it throw errors for undefined variables
+from jinja2 import StrictUndefined
+
+app = Flask(__name__)
+app.secret_key = "dev"
+app.jinja_env.undefined = StrictUndefined
+
+
+
+
+if __name__ == "__main__":
+    # DebugToolbarExtension(app)
+    connect_to_db(app)
+    app.run(host="0.0.0.0", debug=True)
